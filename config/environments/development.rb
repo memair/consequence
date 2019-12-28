84430540e9408411f685d02f814593482acf0ea5
Greg Clarke <greg.clarke@shopify.com>
2019-12-29 05:51:50 +0800

    adding dev url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.hosts << "devconsequence.com"
 
   # Show full error reports.
   config.consider_all_requests_local = true
